2aeb3820b80867ace81d760e38a838a2b8c45b4f
Masaharu TASHIRO <tashiro@leo10.cse.kyutech.ac.jp>
2019-03-03 02:24:55 +0900

    handle EventTypeMessage

--- webapp.go
+++ webapp.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -39,6 +41,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		// var groupID string
 		// var RoomID string
 		var targetID string
+
 		log.Print("EventSource Type: ", event.Source.Type)
 		switch event.Source.Type {
 		case linebot.EventSourceTypeUser:
@@ -52,40 +55,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			targetID = event.Source.RoomID
 		}
 
+		eventSourceType := event.Source.Type
 		replyToken := event.ReplyToken
 
 		log.Print("Event Type: ", event.Type)
 		switch event.Type {
 		case linebot.EventTypeMessage:
-			// Send Current registered addres and confirm resetting
-			var messages []linebot.SendingMessage
-
-			addresses := []string{"a@a.a", "b@b.b", "c@c.c"}
-
-			// Current e-mail addresses
-			var textContents string
-			if len(addresses) > 0 {
-				textContents = "現在転送設定されているメールアドレスは\n" + strings.Join(addresses, "\n") + "\nです"
-			} else {
-				textContents = "現在転送設定されているメールアドレスはありません"
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if strings.Contains(message.Text, "メールお知らせ") || strings.Contains(message.Text, "メールおしらせ") {
+					sendConfirmSetupForwardingMessage(bot, replyToken)
+				}
 			}
-			messages = append(messages, linebot.NewTextMessage(textContents))
-
-			// Confirm template message
-			var altText string
-			if len(addresses) > 0 {
-				altText = "メール転送を再設定しますか？"
-			} else {
-				altText = "メール転送を再設定しますか？"
-			}
-			leftBtn := linebot.NewPostbackAction("はい", "setup=true", "", "はい")
-			rightBtn := linebot.NewPostbackAction("いいえ", "setup=false", "", "いいえ")
-			template := linebot.NewConfirmTemplate(altText, leftBtn, rightBtn)
-			messages = append(messages, linebot.NewTemplateMessage(altText, template))
-
-			// Send messages
-			if _, err := bot.ReplyMessage(replyToken, messages...).Do(); err != nil {
-				log.Print(err)
+			if eventSourceType == linebot.EventSourceTypeUser {
+				sendRandomReply(bot, replyToken)
 			}
 
 		case linebot.EventTypeFollow:
@@ -119,12 +102,53 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 		// }
 	}
+}
 
-	// leftBtn := linebot.NewMessageAction("left", "left clicked")
-	// rightBtn := linebot.NewMessageAction("right", "right clicked")
+func sendConfirmSetupForwardingMessage(bot *linebot.Client, replyToken string) {
+	// Send Current registered addres and confirm resetting
+	var messages []linebot.SendingMessage
 
-	// template := linebot.NewConfirmTemplate("Hello World", leftBtn, rightBtn)
+	addresses := []string{"a@a.a", "b@b.b", "c@c.c"}
 
-	// message := linebot.NewTemplateMessage("Sorry :(, please update your app.", template)
-	w.WriteHeader(200)
+	// Current e-mail addresses
+	var textContents = "こんにちは！メールお知らせくんです。\n"
+	if len(addresses) > 0 {
+		textContents = textContents + "現在転送設定されているメールアドレスは\n" + strings.Join(addresses, "\n") + "\nです"
+	} else {
+		textContents = textContents + "現在転送設定されているメールアドレスはありません"
+	}
+	messages = append(messages, linebot.NewTextMessage(textContents))
+
+	// Confirm template message
+	var altText string
+	if len(addresses) > 0 {
+		altText = "メール転送を再設定しますか？"
+	} else {
+		altText = "メール転送を設定しますか？"
+	}
+	leftBtn := linebot.NewPostbackAction("はい", "setup=true", "", "はい")
+	rightBtn := linebot.NewPostbackAction("いいえ", "setup=false", "", "いいえ")
+	template := linebot.NewConfirmTemplate(altText, leftBtn, rightBtn)
+	messages = append(messages, linebot.NewTemplateMessage(altText, template))
+
+	// Send messages
+	if _, err := bot.ReplyMessage(replyToken, messages...).Do(); err != nil {
+		log.Print(err)
+	}
+}
+
+func sendRandomReply(bot *linebot.Client, replyToken string) {
+	contentPatterns := []string{
+		"ごめんなさい！よく分かりませんでした！",
+		"「メールお知らせくん」と呼んでいただければメール転送設定が確認できます",
+		"新しいメールはありません(たぶん)",
+	}
+	// Randomize reply
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(contentPatterns))
+	message := linebot.NewTextMessage(contentPatterns[i])
+	// Send messages
+	if _, err := bot.ReplyMessage(replyToken, message).Do(); err != nil {
+		log.Print(err)
+	}
 }
