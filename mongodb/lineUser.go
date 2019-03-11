44a467d3b0e906b0a467881d471d080dd7bba33f
Masaharu TASHIRO <tashiro@leo10.cse.kyutech.ac.jp>
2019-03-11 23:59:23 +0900

    WIP: mongodb

---
+++ mongodb/lineUser.go
@@ -0,0 +1,51 @@
+package mongodb
+
+import (
+	"log"
+	"os"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+)
+
+// LineUser ..
+type LineUser struct {
+	LineID              string   `bson:"line_id"`
+	LineName            string   `bson:"line_name"`
+	RegisteredAddresses []string `bson:"registered_address"`
+}
+
+// CreateOrUpdateLineUser ..
+func CreateOrUpdateLineUser(lineUser LineUser) {
+	session, err := mgo.Dial(os.Getenv("MONGODB_URI"))
+	if err != nil {
+		log.Fatal("mgo.Dial: ", err)
+	}
+	defer session.Close()
+
+	db := session.DB("")
+
+	// Insert LineUser
+	col := db.C("LineUser")
+	if err := col.Insert(&lineUser); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// ReadAllLineUsers ..
+func ReadAllLineUsers() []LineUser {
+	session, err := mgo.Dial(os.Getenv("MONGODB_URI"))
+	if err != nil {
+		log.Fatal("mgo.Dial: ", err)
+	}
+	defer session.Close()
+
+	db := session.DB("")
+
+	// Read All LineUsers
+	p := []LineUser{}
+	query := db.C("LineUser").Find(bson.M{})
+	query.All(&p)
+
+	return p
+}
