2c22ca426da344812b7549d891924fefb68a60c4
Masaharu TASHIRO <tashiro@leo10.cse.kyutech.ac.jp>
2019-03-13 02:16:50 +0900

    WIP: Create CheckMailWorker

--- webapp.go
+++ webapp.go
@@ -7,8 +7,8 @@ import (
 	"time"
 
 	"./lineapi"
-	"./mailmanager"
 	"./mongodb"
+	"./workers"
 
 	"github.com/joho/godotenv"
 )
@@ -33,27 +33,5 @@ func DotEnvLoad() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("DotEnv:", err)
-	}
-}
-
-func mailCheck() {
-	mboxName := os.Getenv("IMAP_MBOX_NAME")
-	dateSince := time.Now().AddDate(0, 0, -1)
-	dateBefore := time.Now().AddDate(0, 0, 1)
-
-	messages := mailmanager.FetchMail(dateSince, dateBefore, mboxName, os.Getenv("IMAP_SERVER_NAME"), os.Getenv("IMAP_AUTH_USER"), os.Getenv("IMAP_AUTH_PASSWORD"))
-	//messages := mailmanager.PopMail(dateSince, dateBefore, mboxName, os.Getenv("IMAP_SERVER_NAME"), os.Getenv("IMAP_AUTH_USER"), os.Getenv("IMAP_AUTH_PASSWORD"))
-	log.Println("fetched messages: ", len(messages))
-	// for _, msg := range messages {
-	// 	log.Println(msg.Envelope.Date.String() + ":" + msg.Envelope.Subject)
-	// }
-	if len(messages) > 0 {
-		lineUsers := mongodb.ReadAllLineUsers(os.Getenv("MONGODB_URI"))
-
-		userMailObjects := mailmanager.ConvertMessagesToUserMailObject(messages, lineUsers)
-
-		if len(userMailObjects) > 0 {
-			lineapi.SendPushNotification(userMailObjects)
-		}
 	}
 }
---
+++ workers/MailCheckWorker.go
@@ -0,0 +1,34 @@
+package workers
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"../lineapi"
+	"../mailmanager"
+	"../mongodb"
+)
+
+// MailCheck ..
+func MailCheck() {
+	mboxName := os.Getenv("IMAP_MBOX_NAME")
+	dateSince := time.Now().AddDate(0, 0, -1)
+	dateBefore := time.Now().AddDate(0, 0, 1)
+
+	messages := mailmanager.FetchMail(dateSince, dateBefore, mboxName, os.Getenv("IMAP_SERVER_NAME"), os.Getenv("IMAP_AUTH_USER"), os.Getenv("IMAP_AUTH_PASSWORD"))
+	//messages := mailmanager.PopMail(dateSince, dateBefore, mboxName, os.Getenv("IMAP_SERVER_NAME"), os.Getenv("IMAP_AUTH_USER"), os.Getenv("IMAP_AUTH_PASSWORD"))
+	log.Println("fetched messages: ", len(messages))
+	// for _, msg := range messages {
+	// 	log.Println(msg.Envelope.Date.String() + ":" + msg.Envelope.Subject)
+	// }
+	if len(messages) > 0 {
+		lineUsers := mongodb.ReadAllLineUsers(os.Getenv("MONGODB_URI"))
+
+		userMailObjects := mailmanager.ConvertMessagesToUserMailObject(messages, lineUsers)
+
+		if len(userMailObjects) > 0 {
+			lineapi.SendPushNotification(userMailObjects)
+		}
+	}
+}
