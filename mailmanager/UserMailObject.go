45ec89fb4f1bb1e77ff52c723b2815ffb53c398e
Masaharu TASHIRO <tashiro@leo10.cse.kyutech.ac.jp>
2019-03-13 01:08:31 +0900

    Move UserMailObject to mailmanager package

---
+++ mailmanager/UserMailObject.go
@@ -0,0 +1,73 @@
+package mailmanager
+
+import (
+	"log"
+
+	"../mongodb"
+
+	"github.com/emersion/go-imap"
+)
+
+// MailObject ..
+type MailObject struct {
+	TargetLineID        string
+	MailFromName        string
+	MailFromAddress     string
+	MailReceivedAddress string
+	MailSubject         string
+}
+
+// UserMailObject ..
+type UserMailObject struct {
+	TargetLineID string
+	MailObjects  []MailObject
+}
+
+// ConvertMessagesToUserMailObject ..
+func ConvertMessagesToUserMailObject(messages []imap.Message, lineUsers []mongodb.LineUser) []UserMailObject {
+	var userMailObjects []UserMailObject
+	for _, lineUser := range lineUsers {
+		var mailObjects []MailObject
+	MSG_LOOP:
+		for _, msg := range messages {
+			var addresses []*imap.Address
+			for _, addr := range msg.Envelope.To {
+				addresses = append(addresses, addr)
+			}
+			for _, addr := range msg.Envelope.Cc {
+				addresses = append(addresses, addr)
+			}
+			for _, addr := range msg.Envelope.Bcc {
+				addresses = append(addresses, addr)
+			}
+
+			for _, registeredAddress := range lineUser.RegisteredAddresses {
+				for _, address := range addresses {
+					fullAddress := address.MailboxName + "@" + address.HostName
+					if registeredAddress == fullAddress {
+						mailFromAddress := msg.Envelope.From[0]
+						mailObject := MailObject{
+							TargetLineID:        lineUser.LineID,
+							MailFromName:        mailFromAddress.MailboxName + "@" + mailFromAddress.HostName,
+							MailFromAddress:     mailFromAddress.PersonalName,
+							MailReceivedAddress: fullAddress,
+							MailSubject:         msg.Envelope.Subject,
+						}
+						mailObjects = append(mailObjects, mailObject)
+						log.Println(mailObject)
+						continue MSG_LOOP
+					}
+				}
+			}
+		}
+		if len(mailObjects) > 0 {
+			userMailObject := UserMailObject{
+				TargetLineID: lineUser.LineID,
+				MailObjects:  mailObjects,
+			}
+			userMailObjects = append(userMailObjects, userMailObject)
+		}
+	}
+
+	return userMailObjects
+}
--- webapp.go
+++ webapp.go
@@ -10,29 +10,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/emersion/go-imap"
-
 	"./mailmanager"
 	"./mongodb"
 
 	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
-
-// MailObject ..
-type MailObject struct {
-	TargetLineID        string
-	MailFromName        string
-	MailFromAddress     string
-	MailReceivedAddress string
-	MailSubject         string
-}
-
-// UserMailObject ..
-type UserMailObject struct {
-	TargetLineID string
-	MailObjects  []MailObject
-}
 
 func main() {
 	if len(os.Getenv("DOTENV_LOADED")) < 1 {
@@ -68,7 +51,7 @@ func mailCheck() {
 	if len(messages) > 0 {
 		lineUsers := []LineUser{}
 
-		userMailObjects := ConvertMessagesToUserMailObject(messages, lineUsers)
+		userMailObjects := mailmanager.ConvertMessagesToUserMailObject(messages, lineUsers)
 		log.Println(userMailObjects)
 
 		if len(userMailObjects) > 0 {
@@ -277,7 +260,7 @@ func sendVerificationMail(userName, userAddress, verificationKey string) {
 	mailmanager.SendMail(from, to, subject, body, smptServerName, smtpAuthUser, smtpAuthPassword)
 }
 
-func sendPushNotification(userMailObjects []UserMailObject) {
+func sendPushNotification(userMailObjects []mailmanager.UserMailObject) {
 	//lineChannelID := os.Getenv("LINE_CHANNEL_ID")
 	lineChannelSecret := os.Getenv("LINE_CHANNEL_SECRET")
 	lineAccessToken := os.Getenv("LINE_ACCESS_TOKEN")
@@ -307,53 +290,4 @@ func sendPushNotification(userMailObjects []UserMailObject) {
 		}
 	}
 
-}
-
-// ConvertMessagesToUserMailObject ..
-func ConvertMessagesToUserMailObject(messages []imap.Message, lineUsers []mongodb.LineUser) []UserMailObject {
-	var userMailObjects []UserMailObject
-	for _, lineUser := range lineUsers {
-		var mailObjects []MailObject
-	MSG_LOOP:
-		for _, msg := range messages {
-			var addresses []*imap.Address
-			for _, addr := range msg.Envelope.To {
-				addresses = append(addresses, addr)
-			}
-			for _, addr := range msg.Envelope.Cc {
-				addresses = append(addresses, addr)
-			}
-			for _, addr := range msg.Envelope.Bcc {
-				addresses = append(addresses, addr)
-			}
-
-			for _, registeredAddress := range lineUser.RegisteredAddresses {
-				for _, address := range addresses {
-					fullAddress := address.MailboxName + "@" + address.HostName
-					if registeredAddress == fullAddress {
-						mailFromAddress := msg.Envelope.From[0]
-						mailObject := MailObject{
-							TargetLineID:        lineUser.LineID,
-							MailFromName:        mailFromAddress.MailboxName + "@" + mailFromAddress.HostName,
-							MailFromAddress:     mailFromAddress.PersonalName,
-							MailReceivedAddress: fullAddress,
-							MailSubject:         msg.Envelope.Subject,
-						}
-						mailObjects = append(mailObjects, mailObject)
-						log.Println(mailObject)
-						continue MSG_LOOP
-					}
-				}
-			}
-		}
-		if len(mailObjects) > 0 {
-			userMailObject := UserMailObject{
-				TargetLineID: lineUser.LineID,
-				MailObjects:  mailObjects,
-			}
-			userMailObjects = append(userMailObjects, userMailObject)
-		}
-	}
-
-	return userMailObjects
 }
