18765e83d466dfa8a7bd104dfb0bf25fde764e93
Masaharu TASHIRO <tashiro@leo10.cse.kyutech.ac.jp>
2019-03-19 02:45:50 +0900

    FIX: MailCheck

--- workers/MailCheckWorker.go
+++ workers/MailCheckWorker.go
@@ -14,8 +14,8 @@ import (
 func MailCheck() {
 	configVars := helper.ConfigVars()
 	mboxName := configVars.IMAP.MboxName
-	dateSince := time.Now().AddDate(0, 0, -1)
-	dateBefore := time.Now().AddDate(0, 0, 1)
+	dateSince := time.Now().AddDate(0, 0, -2)
+	dateBefore := time.Now().AddDate(0, 0, 2)
 
 	messages := mailmanager.FetchMail(dateSince, dateBefore, mboxName, configVars.IMAP.ServerName, configVars.IMAP.AuthUser, configVars.IMAP.AuthPassword)
 	//messages := mailmanager.PopMail(dateSince, dateBefore, mboxName, configVars.IMAP.ServerName, configVars.IMAP.AuthUser, configVars.IMAP.AuthPassword)
