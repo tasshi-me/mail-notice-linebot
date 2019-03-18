54b407e0f1acdc3ba7cc216482da3f53178a6e2c
Masaharu TASHIRO <tashiro@leo10.cse.kyutech.ac.jp>
2019-03-19 02:48:15 +0900

    Add log Print

--- webapp.go
+++ webapp.go
@@ -33,11 +33,13 @@ func main() {
 		interval := 20 * time.Minute
 		appURL := "https://" + herokuAppName + ".herokuapp.com/"
 		go workers.KeepAliveWorker(interval, appURL)
+		log.Println("Start Keep-Alive Worker for Heroku")
 	}
 
 	// Start MailCheckWorker
 	interval := 5 * time.Minute
 	go workers.MailCheckWorker(interval)
+	log.Println("Start MailCheck Worker")
 
 	// Start http server for linebot webhook
 	port := configVars.Port
